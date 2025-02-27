9339ecbbc2f7041ee5e463babee38dece0b7ea34
jungwook0103 <0103jungwook@gmail.com>
2025-02-27 12:05:07 +0900

    spring

---
+++ ezen/src/main/java/com/kjw/EzenApplication.java
@@ -0,0 +1,13 @@
+package com.kjw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzenApplication.class, args);
+	}
+
+}
---
+++ ezen/src/test/java/com/kjw/EzenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
